c32fb0eda1ac973fad564f23b9cedf6502484733
Patrick Murphy <pmurphy1413@gmail.com>
2021-09-17 10:16:57 -0500

    First commit

---
+++ app/src/main/java/com/example/myapplication/InvalidActivity.java
@@ -0,0 +1,55 @@
+package com.example.myapplication;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+//import android.support.v4.app.ActivityCompat;
+//import android.support.v7.app.AppCompatActivity;
+import android.telephony.TelephonyManager;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+public class InvalidActivity extends AppCompatActivity {
+
+    private static int REQUEST_CODE=100;
+    private TextView invalidTV;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invalid);
+
+        invalidTV = (TextView) findViewById(R.id.invalidTV);
+
+        Intent receivedIntent = getIntent();
+        if (receivedIntent != null){
+            invalidTV.setText("Username and password do not match.");
+
+            String username = receivedIntent.getStringExtra("U");
+            String password = receivedIntent.getStringExtra("P");
+            //obtain the device unique ID
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+                    checkSelfPermission(Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_PHONE_STATE}, REQUEST_CODE);
+            }else {
+
+                TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+                String deviceID = telephonyManager.getDeviceId();
+                //send an Intent with U, P, and DeviceID extra data
+                Intent serviceIntent = new Intent("A3");
+                serviceIntent.setPackage(this.getPackageName());
+                serviceIntent.putExtra("U", username);
+                serviceIntent.putExtra("P", password);
+                serviceIntent.putExtra("DeviceID", deviceID);
+                startService(serviceIntent);
+            }
+
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/ValidActivity.java
@@ -0,0 +1,37 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+//import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ValidActivity extends AppCompatActivity {
+
+    private TextView validTV;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_valid);
+
+        validTV = (TextView) findViewById(R.id.validTV);
+        //get the extra data from the received Intent
+
+        Intent receivedIntent = getIntent();
+        if(receivedIntent != null){
+            //get the username
+            String username = receivedIntent.getStringExtra("U");
+            validTV.setText("You have successfully logged in.");
+            //send broadcast Intent
+            Intent broadcastIntent = new Intent("A2");
+            broadcastIntent.putExtra("U", username);
+            sendBroadcast(broadcastIntent);
+        }
+
+
+
+
+
+    }
+}
